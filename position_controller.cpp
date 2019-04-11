4e56ecfedad5d178d9c04c79a423c830cfb53195
Rijesh Augustine <rijesh@ualberta.ca>
2019-04-11 17:01:29 -0600

    added set_attitude_target message to position controller

--- position_controller.cpp
+++ position_controller.cpp
@@ -4,6 +4,14 @@ int system_id = 1;
 int autopilot_id = 1;
 int companion_id = 0;
 
+float Position_Controller::bind_max_value(float val, float max_val, float min_val){
+    if (val > max_val)
+        return max_val;
+    else if (val < min_val)
+        return min_val;
+    return val;
+}
+
 Position_Controller::Position_Controller(Multithreaded_Interface *mti)
 {
 	this->mti = mti;
@@ -11,7 +19,7 @@ Position_Controller::Position_Controller(Multithreaded_Interface *mti)
 	desired_position.type_mask = MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_POSITION;
 	desired_position.coordinate_frame = MAV_FRAME_LOCAL_NED;
 
-	desired_position_periodic = new Periodic_Message(mti, desired_position_message, 10);
+	//desired_position_periodic = new Periodic_Message(mti, desired_position_message, 10);
 
 	update_current_position(0, 0, 0, 0);
 	update_desired_position(0, 0, 0, 0);
@@ -39,6 +47,34 @@ void Position_Controller::update_desired_position(float x, float y, float z, flo
 
 	mavlink_msg_set_position_target_local_ned_encode(system_id, companion_id, &desired_position_message, &desired_position);
 	desired_position_periodic->update_message(desired_position_message);
+}
+
+void Position_Controller::update_attitude_target(float pitch_target, float roll_target, float yaw_target, float vel_z)
+{
+	float cr2 = cosf(roll_target*M_PI/180.0f*0.5f);
+	float cp2 = cosf(pitch_target*M_PI/180.0f*0.5f);
+	float cy2 = cosf(yaw_target*M_PI/180.0f*0.5f);
+	float sr2 = sinf(roll_target*M_PI/180.0f*0.5f);
+	float sp2 = sinf(pitch_target*M_PI/180.0f*0.5f);
+	float sy2 = sinf(yaw_target*M_PI/180.0f*0.5f);
+
+	attitude_target.q[0] = cr2*cp2*cy2 + sr2*sp2*sy2;
+	attitude_target.q[1] = sr2*cp2*cy2 - cr2*sp2*sy2;
+	attitude_target.q[2] = cr2*sp2*cy2 + sr2*cp2*sy2;
+	attitude_target.q[3] = cr2*cp2*sy2 - sr2*sp2*cy2;
+
+	float thrust = (vel_z*100)/(2.0*DEFAULT_SPEED_UP) + 0.5;
+	float thrust = bind_max_value(thrust,1,0);
+
+	attitude_target.thrust = thrust;
+
+	attitude_target.target_system = 1;
+	attitude_target.target_component = 0;
+
+	attitude_target.type_mask = 0x7;
+
+	mavlink_msg_set_attitude_target_encode(system_id, companion_id, &attitude_target_message, &attitude_target);
+	mti->write_message(attitude_target_message);
 }
 
 void Position_Controller::toggle_offboard_control(bool flag)
--- position_controller.h
+++ position_controller.h
@@ -6,22 +6,30 @@
 
 #define MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_POSITION 0b0000101111111000
 
+#define DEFAULT_SPEED_UP 10 //cm/s
+
 class Position_Controller
 {
 public:
   Position_Controller(Multithreaded_Interface *mti);
   Multithreaded_Interface *mti;
+  
+  float bind_max_value(float val, float max_val, float min_val);
 
   mavlink_vision_position_estimate_t current_position;
   mavlink_set_position_target_local_ned_t desired_position;
+  mavlink_set_attitude_target_t attitude_target;
 
   mavlink_message_t current_position_message;
   mavlink_message_t desired_position_message;
+  mavlink_message_t attitude_target_message;
 
-  Periodic_Message *desired_position_periodic;
+  //Periodic_Message *desired_position_periodic;
 
   void update_current_position(float x, float y, float z, float yaw);
   void update_desired_position(float x, float y, float z, float yaw);
+  
+  void update_attitude_target(float pitch_target, float roll_target, float yaw_target, float vel_z);
   
   void toggle_offboard_control(bool flag);
 
